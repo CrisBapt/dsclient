db323ea50cffb974990bca4e8a428ffa39b18fc4
Cristina Baptista <1210489@isep.ipp.pt>
2023-11-01 23:54:11 +0000

    Exceptions, Find Client by id

--- dsclient/src/main/java/com/dsclient/dsclient/services/ClientService.java
+++ dsclient/src/main/java/com/dsclient/dsclient/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.dsclient.dsclient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dsclient.dsclient.dtos.ClientDTO;
 import com.dsclient.dsclient.entities.Client;
 import com.dsclient.dsclient.repositories.ClientRepository;
+import com.dsclient.dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -23,5 +25,16 @@ public class ClientService {
 		
 		return clients.stream().map(c -> new ClientDTO(c)).collect(Collectors.toList());
 	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		
+		Client client = obj.orElseThrow(() -> new ResourceNotFoundException("ENtity Not Found")); 
+		
+		return new ClientDTO(client);
+	}
+	
+	
 	
 }
